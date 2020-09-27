0880d4175d1b1a2fdc14a4a216d1ae1e98c01a68
Tomas <tjelvez@uc.cl>
2020-09-27 16:13:05 -0300

    consultas

--- Sites/index.php
+++ Sites/index.php
@@ -14,10 +14,10 @@
   </form>
 
 
-  <h3 align="center"> Buscar buque por el nombre de la naviera </h3>
+  <h3 align="center"> Buscar buques por el nombre de la naviera </h3>
 
   <form align="center" action="consultas/consulta_tipo_nombre.php" method="post">
-    Nombre de la Naviera:
+    Buques de la naviera:
     <input type="text" name="nombre_naviera">
     <br/>
     <br/>
@@ -31,10 +31,10 @@
   <h3 align="center">Buscar por puerto y año de atraque</h3>
 
   <form align="center" action="consultas/consulta_puerto_año.php" method="post">
-    Puerto:
+    Buques que hayan atracado en:
     <input type="text" name="puerto">
     <br/>
-    Año:
+    el año:
     <input type="text" name="año">
     <br/><br/>
     <input type="submit" value="Buscar">
@@ -68,43 +68,10 @@
     <input type="submit" value="Buscar">
   </form>
   <br>
-  <br>
-  <br>
   <h3 align="center"> Buscar buque pesquero con mayor numero de personas trabajando </h3>
-
 <form align="center" action="consultas/consulta_pesquero.php" method="post">
-  <br/>
-  <br/>
   <input type="submit" value="Buscar">
 </form>
 
-  <h3 align="center">¿Quieres buscar todos los pokemones por tipo?</h3>
-
-  <?php
-  #Primero obtenemos todos los tipos de pokemones
-  require("config/conexion.php");
-  $result = $db -> prepare("SELECT DISTINCT tipo FROM pokemones;");
-  $result -> execute();
-  $dataCollected = $result -> fetchAll();
-  ?>
-
-  <form align="center" action="consultas/consulta_tipo.php" method="post">
-    Seleccinar un tipo:
-    <select name="tipo">
-      <?php
-      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-      foreach ($dataCollected as $d) {
-        echo "<option value=$d[0]>$d[0]</option>";
-      }
-      ?>
-    </select>
-    <br><br>
-    <input type="submit" value="Buscar por tipo">
-  </form>
-
-  <br>
-  <br>
-  <br>
-  <br>
 </body>
 </html>
