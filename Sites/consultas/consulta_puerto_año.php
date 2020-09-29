3f8a127393863a32893c90cec9358a8a9f7a30a3
Tomas <tjelvez@uc.cl>
2020-09-29 10:59:43 -0300

    consulta3

--- Sites/consultas/consulta_puerto_año.php
+++ Sites/consultas/consulta_puerto_año.php
@@ -6,26 +6,28 @@
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../config/conexion.php");
 
-  $nombre = $_POST["puerto"];
+  $puerto = $_POST["puerto"];
   $año = $_POST["año"];
 
- 	$query = "SELECT * FROM naviera WHERE tipo LIKE '%$tipo%' AND nombre LIKE '%$nombre%';";
-	$result = $db -> prepare($query);
-	$result -> execute();
-	$buques = $result -> fetchAll();
-  ?>
-
-	<table>
-    <tr>
-      <th>ID</th>
-      <th>Nombre</th>
-      <th>Tipo</th>
-    </tr>
-  <?php
-	foreach ($pokemones as $pokemon) {
-  		echo "<tr> <td>$pokemon[0]</td> <td>$pokemon[1]</td> <td>$pokemon[2]</td> </tr>";
-	}
-  ?>
-	</table>
+ 	$query = "SELECT * FROM buques.bid,nombre,patente,pais,capitan WHERE atraques.bid = buques.bid AND LOWER(atraques.puerto) LIKE LOWER('%$puerto%') AND EXTRACT(year FROM fecha_atraque)= $año;";
+   $result = $db -> prepare($query);
+   $result -> execute();
+   $buques = $result -> fetchAll();
+   ?>
+ 
+   <table>
+     <tr>
+       <th>ID</th>
+       <th>Nombre</th>
+       <th>Patente</th>
+       <th>Pais</th>
+       <th>ID_Capitan</th>
+     </tr>
+   <?php
+   foreach ($buques as $buque) {
+       echo "<tr> <td>$buque[0]</td> <td>$buque[1]</td> <td>$buque[2]</td><td>$buque[3]</td><td>$buque[4]</td> </tr>";
+   }
+   ?>
+   </table>
 
 <?php include('../templates/footer.html'); ?>
