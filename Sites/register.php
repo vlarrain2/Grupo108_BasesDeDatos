d3c4a0a4acd6a544dc5a8774480dc98a4051037e
Tomas <tjelvez@uc.cl>
2020-10-19 18:25:58 -0300

    register.php

--- Sites/register.php
+++ Sites/register.php
@@ -1,12 +1,28 @@
 <form method="post" action="" name="signup-form">
     <div class="form-element">
-        <label>Username</label>
-        <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+        <label>Nombre</label>
+        <input type="text" name="nombre" pattern="[a-zA-Z0-9]+" required />
     </div>
     <div class="form-element">
-        <label>Email</label>
-        <input type="email" name="email" required />
+        <label>Edad</label>
+        <input type="number" name="edad" required />
     </div>
+
+    <div class="form-element">
+        <label>Sexo</label>
+        <input type="sexo" name="sexo" required />
+    </div>
+
+    <div class="form-element">
+        <label>Numero de Pasaporte</label>
+        <input type="number" name="pasaporte" required />
+    </div>
+
+    <div class="form-element">
+        <label>Nacionalidad</label>
+        <input type="text" name="nacionalidad" required />
+    </div>
+
     <div class="form-element">
         <label>Password</label>
         <input type="password" name="password" required />
